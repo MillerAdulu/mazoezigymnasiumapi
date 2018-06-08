b37eec94acdb950662ca81034954c2a81b16c252
Miller Adulu <milleradulu@gmail.com>
2018-06-08 22:52:08 +0300

    Format all gym locations in the standard gym instructor resource format

--- app/Http/Controllers/GymInstructorController.php
+++ app/Http/Controllers/GymInstructorController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Gyminstructor;
 use App\Http\Resources\GymInstructorCollection;
+use App\Http\Resources\GymInstructor as GymInstructorResource;
 use Illuminate\Http\Request;
 
 class GymInstructorController extends Controller
 {
     public function index(){
-        return new GymInstructorCollection(Gyminstructor::all());
+        return new GymInstructorCollection(GymInstructorResource::collection(Gyminstructor::all()));
+    }
+
+    public function instructor($id){
+        return new GymInstructorResource(Gyminstructor::findOrFail($id));
     }
 }
