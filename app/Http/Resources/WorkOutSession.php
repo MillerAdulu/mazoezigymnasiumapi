c79fa5da3851ac325a1ec5bdfd147abe92758cf0
Miller Adulu <milleradulu@gmail.com>
2018-06-14 00:58:32 +0300

    Return resolved exercises and locations

--- app/Http/Resources/WorkOutSession.php
+++ app/Http/Resources/WorkOutSession.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Exercise;
+use App\Gymlocation;
 
 class WorkOutSession extends JsonResource
 {
@@ -14,6 +16,28 @@ class WorkOutSession extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $locations = Gymlocation::all(['id', 'location']);
+        $exercises = Exercise::all(['id', 'name']);
+        $session_location = "";
+        $exercise_type = "";
+
+        foreach ($locations as $location) :
+            if($location->id == $this->location) {
+                $session_location = $location->location;
+            }
+        endforeach;
+
+        foreach ($exercises as $exercise) :
+            if($exercise->id == $this->exercise_type) {
+                $exercise_type = $exercise->name;
+            }
+        endforeach;
+
+        return [
+            'exercise_type' => $exercise_type,
+            'location' => $session_location,
+            'sets' => $this->sets,
+            'reps' => $this->reps,
+        ];
     }
 }
